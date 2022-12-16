2f60b9a9c41ca90d66badc512ddac9e48e60a3ea
AbdulRehman7269 <syedabdulrehman7269@gmail.com>
2022-12-16 12:37:47 +0500

    changes in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -11,23 +11,28 @@ class JazzWorld extends StatelessWidget {
       home:        Scaffold(
         appBar:  AppBar(
           backgroundColor: Colors.red[800],
-          leading: const IconButton(onPressed: null, icon: Icon(Icons.notifications_none, color: Colors.white,),
-          ),
+
 
           title: Row(children:const [
             Expanded(child: IconButton(
-              padding: EdgeInsets.only(right: 350),
+              padding: EdgeInsets.only(right: 20),
+              onPressed: null, icon: Icon(Icons.notifications_none, color: Colors.white,),
+            ),
+
+            ),
+            IconButton(
+              padding: EdgeInsets.only(right: 40),
               onPressed: null, icon: Icon(Icons.card_giftcard, color: Colors.white,),
             ),
-            ),
             Text('Jazz World' ,
-              style: TextStyle(fontSize: 22, ), textAlign: TextAlign.center,  ),
+              style: TextStyle(fontSize: 22,),  ),
 
           ],
           ),
+          //  leading: const IconButton(onPressed: null, icon: Icon(Icons.notifications_none, color: Colors.white,),
+          // padding: EdgeInsets.only(left: 0), ),
           actions: const [
             IconButton(
-              padding: EdgeInsets.only(left: 30),
               onPressed: null, icon: Icon(Icons.refresh, color: Colors.white,),
             ),
             IconButton(onPressed: null, icon: Icon(Icons.search, color: Colors.white,),
@@ -36,6 +41,7 @@ class JazzWorld extends StatelessWidget {
             ),
           ],
         ),
+
       ),
     );
 
